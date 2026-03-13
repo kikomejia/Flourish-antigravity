199e67f135810349ed29c0b023d4a24e0b6b89bb
fmu <fmu@me.com>
2026-03-13 19:20:55 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import { Toaster } from "@/components/ui/toaster"
+
+const fontStyle = `
+  @import url('https://api.fontshare.com/v2/css?f[]=recoleta@400,500,700&display=swap');
+  * { font-family: 'Recoleta', serif !important; }
+`;
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
