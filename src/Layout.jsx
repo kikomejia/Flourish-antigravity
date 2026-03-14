36e0b5a214c735d4055a4af684de4c769e556e86
fmu <fmu@me.com>
2026-03-14 04:49:51 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -7,7 +7,20 @@ export default function Layout({ children }) {
         html, body, #root {
           background-color: #050508 !important;
           min-height: 100%;
+          overscroll-behavior: none;
         }
+        body::before, body::after {
+          content: '';
+          display: block;
+          position: fixed;
+          left: 0;
+          right: 0;
+          height: 50vh;
+          background-color: #050508;
+          z-index: -1;
+        }
+        body::before { top: -50vh; }
+        body::after { bottom: -50vh; }
       `}</style>
       {children}
     </>
