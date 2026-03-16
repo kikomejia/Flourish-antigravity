91b541903c4c53048c122236fa0419a6e1f729cb
fmu <fmu@me.com>
2026-03-16 19:25:08 +0000

    File changes

--- src/components/VirtueHexagon.jsx
+++ src/components/VirtueHexagon.jsx
@@ -61,6 +61,14 @@ export default function VirtueHexagon({ completedVirtues = [], acceptedVirtues =
           ))}
         </defs>
 
+        {/* Pulsing glow circle in center when a virtue is active */}
+        {activeVirtueData && (
+          <circle cx="2333" cy="2548" r="900" fill={activeVirtueData.color} opacity="0" style={{ pointerEvents: "none" }}>
+            <animate attributeName="opacity" values="0;0.12;0" dur="2s" repeatCount="indefinite" />
+            <animate attributeName="r" values="700;1050;700" dur="2s" repeatCount="indefinite" />
+          </circle>
+        )}
+
         {PETALS.map((petal) => {
           const virtue = VIRTUES.find((v) => v.key === petal.key);
           const isCompleted = completedVirtues.includes(petal.key);
